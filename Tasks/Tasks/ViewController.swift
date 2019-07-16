a28ce49e4baa51c46352b63bf0edf36026d2c7ba
max <maximohinojosa24@yahoo.com>
2019-07-16 11:42:05 -0700

    [add] taskApp initial start

---
+++ Tasks/Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Student Loaner 3 on 7/16/19.
+//  Copyright © 2019 Student Loaner 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
